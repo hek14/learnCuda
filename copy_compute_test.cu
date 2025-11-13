5b9f990be4e035c1e18209737f2f3e299fa6cb66
hek14 <1023129548@qq.com>
2025-11-13 10:33:13 +0800

    remove exeperiment_2

--- copy_compute_test.cu
+++ copy_compute_test.cu
@@ -71,77 +71,12 @@ void run_experiment(int num_copies, size_t chunk_size, int kernel_repeat) {
     delete[] h_bufs;
 }
 
-
-void run_experiment_2(int num_copies, size_t chunk_size, int kernel_repeat) {
-    float **d_bufs = new float*[num_copies];
-    float **h_bufs = new float*[num_copies];
-    cudaStream_t copy_stream, compute_stream;
-    cudaStreamCreateWithFlags(&copy_stream, cudaStreamNonBlocking);
-    cudaStreamCreateWithFlags(&compute_stream, cudaStreamNonBlocking);
-
-    // Allocate device/host buffers
-    for (int i = 0; i < num_copies; ++i) {
-        cudaMalloc(&d_bufs[i], chunk_size * sizeof(float));
-        cudaMallocHost(&h_bufs[i], chunk_size * sizeof(float));
-    }
-
-    // Prepare compute buffer
-    float *d_compute;
-    cudaMalloc(&d_compute, chunk_size * sizeof(float));
-
-    // Init compute buffer
-    cudaMemsetAsync(d_compute, 0, chunk_size * sizeof(float), compute_stream);
-    cudaStreamSynchronize(compute_stream);
-
-    // Start timing
-    cudaEvent_t start, end;
-    cudaEventCreate(&start);
-    cudaEventCreate(&end);
-    cudaEventRecord(start, copy_stream);
-
-    // Enqueue many DtoH copies (device → host) in copy_stream
-    for (int i = 0; i < num_copies; ++i)
-        cudaMemcpyAsync(h_bufs[i], d_bufs[i], chunk_size * sizeof(float), cudaMemcpyDeviceToHost, copy_stream);
-
-    heavy_compute<<<chunk_size/256, 256, 0, compute_stream>>>(d_compute, chunk_size, kernel_repeat);
-
-    // cudaStreamSynchronize(copy_stream);
-    cudaStreamSynchronize(compute_stream);
-    cudaEventRecord(end, copy_stream);
-    cudaEventSynchronize(end);
-
-
-    // Get timing (ms)
-    float total_ms = 0.0f;
-    cudaEventElapsedTime(&total_ms, start, end);
-
-    printf("copies: %d,\ttotal time: %f ms\n", num_copies, total_ms);
-
-    // Cleanup
-    for (int i = 0; i < num_copies; ++i) {
-        cudaFree(d_bufs[i]);
-        cudaFreeHost(h_bufs[i]);
-    }
-    cudaFree(d_compute);
-    cudaStreamDestroy(copy_stream);
-    cudaStreamDestroy(compute_stream);
-    cudaEventDestroy(start);
-    cudaEventDestroy(end);
-    delete[] d_bufs;
-    delete[] h_bufs;
-}
-
-
 int main() {
     size_t chunk_size = 1 << 12;    // 4096 floats (~16 KB)
     int kernel_repeat = 500000;     // Controls kernel workload
 
     // Test for different # of copies, observe timing
     int copy_counts[] = {0, 0, 1, 100, 1000, 2000, 5000, 10000};
-    printf("==========\ntime spent including kernel launch\n");
-    for (int i = 0; i < sizeof(copy_counts)/sizeof(copy_counts[0]); ++i)
-        run_experiment_2(copy_counts[i], chunk_size, kernel_repeat);
-
     printf("==========\ntime spent excluding kernel launch\n");
     for (int i = 0; i < sizeof(copy_counts)/sizeof(copy_counts[0]); ++i)
         run_experiment(copy_counts[i], chunk_size, kernel_repeat);
